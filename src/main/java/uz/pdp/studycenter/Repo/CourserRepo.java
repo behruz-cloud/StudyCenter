13f49c5424ad92b4d68f33387cd533a756d7e685
Behruzbek Mahmudjonov <behruzbekmahmudjonov08@gmail.com>
2024-12-03 12:21:41 +0500

    Add Report page

--- src/main/java/uz/pdp/studycenter/Repo/CourserRepo.java
+++ src/main/java/uz/pdp/studycenter/Repo/CourserRepo.java
@@ -2,7 +2,9 @@ package uz.pdp.studycenter.Repo;
 
 import jakarta.persistence.EntityManager;
 import uz.pdp.studycenter.entity.Course;
+import uz.pdp.studycenter.entity.Report;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static uz.pdp.studycenter.myListener.MyListener.EMF;
@@ -19,4 +21,20 @@ public class CourserRepo {
         }
 
     }
+
+    public static List<Object[]> getReport() {
+        try (EntityManager em = EMF.createEntityManager()) {
+            String query = """
+                select c.name, count(s.id), sum(s.amount)
+                from Course c
+                         join Module m on m.course = c
+                         join Groups g on g.module = m
+                         join Student s on s.groups = g
+                group by c.name
+                """;
+
+            return em.createQuery(query, Object[].class).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }}
 }
